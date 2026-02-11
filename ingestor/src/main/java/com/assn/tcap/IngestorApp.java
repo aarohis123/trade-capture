75c5506dc6e041a0ad18ed67c57ca767cac18307
aarohi <aarohis123@gmail.com>
2026-02-11 08:47:14 +0530

    Project Setup Commit

---
+++ ingestor/src/main/java/com/assn/tcap/IngestorApp.java
@@ -0,0 +1,11 @@
+package com.assn.tcap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestorApp {
+    public static void main(String[] args) {
+        SpringApplication.run(IngestorApp.class, args);
+    }
+}
